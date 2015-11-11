db37082afc78381c8c80ec0bfcfa076cfdb219fe
FuyuSyogun <fuyu-syogun@outlook.com>
2015-11-11 11:21:10 +0800

    get_distance in views / distance in model

--- apis/views.py
+++ apis/views.py
@@ -10,7 +10,14 @@ import json
 from django.utils.encoding import smart_str
 from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.contrib.auth.models import User
 
 
+def get_distance(request):
+    from_time = request.GET["from_time"]
+    to_time = request.GET["to_time"]
+    entries = request.user.st_user.filter(st_time__range=(from_time, to_time))
+    return JsonResponse({'entries': entries})
 
 @csrf_exempt
