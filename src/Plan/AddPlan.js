8eb617e1b11821a61ea9a0de01ada02be1dd730c
Fahu Wen <wenfahu@outlook.com>
2015-11-26 14:34:06 +0800

    rm .idea

--- src/Plan/AddPlan.js
+++ src/Plan/AddPlan.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 
-export default class Login extends Component {
+export default class AddPlan extends Component{
     render(){
-	<h1> Login </h1>
     }
 }
---
+++ src/Plan/Plans.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class Plans extends Component {
+    render(){
+	<h1> Plans </h1>
+    }
+}
